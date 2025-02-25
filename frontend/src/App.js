1ba33ce345141ca74417a3e06a3de019b6714770
Sarah <s.alramahi246@gmail.com>
2025-02-25 13:10:36 -0600

    navbar added and  page folder setup done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar";
+// import Home from "./pages/home";
+// import SearchBooks from "./pages/searchbooks";
+// import Chats from "./pages/chat";
+// import Profile from "./pages/profile";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Routes>
+          {/* <Route path="/" element={<Home />} />
+          <Route path="/search" element={<SearchBooks />} />
+          <Route path="/chats" element={<Chats />} />
+          <Route path="/profile" element={<Profile />} /> */}
+        </Routes>
+        <Navbar />
+      </div>
+    </Router>
   );
 }
 
