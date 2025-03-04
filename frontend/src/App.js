ef9f629d3cee8b67641abff421d5e6fdae6375fe
Sarah <s.alramahi246@gmail.com>
2025-03-04 03:10:27 -0600

    Front end shells, intro page, dummy login page and stlying almost done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
 import Home from "./pages/home";
 import SearchBooks from "./pages/searchbooks";
-// import Chats from "./pages/chat";
+import Chats from "./pages/chat";
 import Profile from "./pages/profile";
+import Intro from "./pages/intro";
+import Login from "./pages/login";
 
 function App() {
   return (
@@ -12,14 +14,14 @@ function App() {
       <div className="app">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/intro" element={<Intro />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/search" element={<SearchBooks />} />
-          {/* <Route path="/" element={<Home />} />
-          <Route path="/search" element={<SearchBooks />} />
           <Route path="/chats" element={<Chats />} />
-          <Route path="/profile" element={<Profile />} /> */}
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/nav" element={<Navbar />} />
         </Routes>
-        <Navbar />
       </div>
     </Router>
   );
