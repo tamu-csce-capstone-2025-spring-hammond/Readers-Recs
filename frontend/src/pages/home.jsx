8ced9300f6981147e141ea0f8288a29095b91dfa
annapachecohartman <ahartman03@tamu.edu>
2025-04-06 21:43:43 -0500

    book deletion functionality!

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import {ClipLoader} from 'react-spinners';
 import { Link } from 'react-router-dom';
 import { ChevronRight, BookOpen, Clock, Award, PlusCircle, ThumbsUp, ThumbsDown, Minus } from 'lucide-react';
 import Navbar from '../components/navbar';
